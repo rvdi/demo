5499a117fe560081d822f19a8af3f9f6e9d4d8d5
Rudi Kristanto <rudi.kristanto@fibronostics.com>
2022-08-31 13:12:24 +0800

    use app service

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,9 @@
 package com.example.demo;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -23,35 +15,10 @@ public class DemoApplication {
 }
 
 @RestController
-class CosmosController {
-
-	private final ReservationRepository reservationRepository;
-
-	private int counter = 0;
-
-	public CosmosController(ReservationRepository rr) {
-		reservationRepository = rr;
-	}
+class HelloWorldController {
 
 	@GetMapping("/hello-world")
 	String helloWorld() {
-		reservationRepository.save(new Reservation(null, "Test azure cosmos " + counter++));
-
-		return reservationRepository.findAll()
-				.stream()
-				.map(Reservation::getName)
-				.collect(Collectors.joining());
+		return "Hello world Azure";
 	}
-}
-
-interface ReservationRepository extends MongoRepository<Reservation, String> {}
-
-@Document
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-class Reservation{
-	@Id
-	private String id;
-	private String name;
 }
